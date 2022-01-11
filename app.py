1c70875b5cd7df34d39d23e2161fa74d7ce8d47e
Amine AKROUT <akrout.med.amine@gmail.com>
2022-01-11 13:11:26 +0100

    update port

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def render_message():
     return render_template('index.html' ,message=message)
 
 if __name__ == '__main__':
-    app.run(debug=True , host='localhost', port=8080)
-    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    # app.run(debug=True , host='localhost', port=8080)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
